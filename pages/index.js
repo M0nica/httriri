436af0f26aee57f781267ebf6dbac51d7919815e
Monica Powell <monica@aboutmonica.com>
2020-06-12 18:28:07 -0400

    add meta data for twitter

--- pages/index.js
+++ pages/index.js
@@ -34,12 +34,22 @@ export default function Home() {
           <meta property="og:title" content="HTTRiRi" />
           <title>HTTRiRi</title>
           <link rel="icon" href="/favicon.ico" />
-          <meta property="og:image" content="/homepage-screenshot.png" />
+          <meta
+            property="og:image"
+            content="https://httriri.com/homepage-screenshot.png"
+          />
           <meta property="og:url" content="https://httriri.com" />
           <meta
             property="og:description"
             content="Explore HTTP status codes with this collection of Rihanna GIFs"
           />
+
+          <meta property="twitter:title" content="HTTRiRi" />
+          <meta
+            property="twitter:image"
+            content="https://httriri.com/homepage-screenshot.png"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
         </Head>
 
         <main>
