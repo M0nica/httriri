d11aa59b8aac8625251e02455cad4b473c597078
Monica Powell <monica@aboutmonica.com>
2020-06-13 10:35:18 -0400

    remove uncecessary paragraph tag

--- components/statusCodeCard.tsx
+++ components/statusCodeCard.tsx
@@ -3,9 +3,8 @@ import styles from "./statusCodeCard.module.css";
 export default function StatusCodeCard({ code, title }) {
   return (
     <div className={styles.card}>
-      <p>
-        <img className={styles.gif} src={`/images/${code}.gif`} />
-      </p>
+      <img className={styles.gif} src={`/images/${code}.gif`} />
+
       <a className={styles.link} href={`https://httpstatuses.com/${code}`}>
         {code}: {title} &#x2197;
       </a>
