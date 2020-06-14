7b6f8d28bb824193921bdef1cfd3171b495c03a9
Monica Powell <monica@aboutmonica.com>
2020-06-13 20:12:44 -0400

    add prelim alt tag descriptions

--- components/statusCodeCard.tsx
+++ components/statusCodeCard.tsx
@@ -1,9 +1,13 @@
 import styles from "./statusCodeCard.module.css";
 
-export default function StatusCodeCard({ code, title }) {
+export default function StatusCodeCard({ code, title, description }) {
   return (
     <div className={styles.card}>
-      <img className={styles.gif} src={`/images/${code}.gif`} />
+      <img
+        className={styles.gif}
+        src={`/images/${code}.gif`}
+        alt={description}
+      />
 
       <a className={styles.link} href={`https://httpstatuses.com/${code}`}>
         {code}: {title} &#x2197;
--- utils/statuses.ts
+++ utils/statuses.ts
@@ -1,21 +1,85 @@
 const statuses = [
-  { code: 100, title: "Continue" },
-  { code: 101, title: "Switching Protocols" },
-  { code: 200, title: "OK" },
-  { code: 202, title: "Accepted" },
-  { code: 300, title: "Multiple Choices" },
-  { code: 301, title: "Moved Permanently" },
-  { code: 400, title: "Bad Request" },
-  { code: 402, title: "Payment Required" },
-  { code: 403, title: "Forbidden" },
-  { code: 406, title: "Not Acceptable" },
-  { code: 407, title: "Proxy Authentication Required" },
-  { code: 409, title: "Conflict" },
-  { code: 410, title: "Gone" },
-  { code: 418, title: "I'm a teapot" },
-  { code: 426, title: "Upgrade required" },
-  { code: 429, title: "Too many requests" },
-  { code: 503, title: "Service unavailable" },
+  {
+    code: 100,
+    title: "Continue",
+    description: "Rihanna with red hair asking if 'You ready?'",
+  },
+  {
+    code: 101,
+    title: "Switching Protocols",
+    description: "Rihanna splashing water around in a music video",
+  },
+  {
+    code: 200,
+    title: "OK",
+    description:
+      "Rihanna nodding her head while cheering on Lizzo at the BET Awards 2019",
+  },
+  { code: 202, title: "Accepted", description: "Rihanna with a bob winking" },
+  {
+    code: 300,
+    title: "Multiple Choices",
+    description:
+      "Rihanna dancing on stage with a pink outfit with an ensemble of background dancers behind her wearing outfits with the same shade of pink.",
+  },
+  {
+    code: 301,
+    title: "Moved Permanently",
+    description: "Rihanna peacefully sipping from a coconut",
+  },
+  {
+    code: 400,
+    title: "Bad Request",
+    description: "Rihanna shaking her finger 'no'",
+  },
+  {
+    code: 402,
+    title: "Payment Required",
+    description: "Rihanna making hand gestures for money",
+  },
+  {
+    code: 403,
+    title: "Forbidden",
+    description:
+      "Rihanna rolling her eyes at some one before her limo window rolls up",
+  },
+  {
+    code: 406,
+    title: "Not Acceptable",
+    description: "Rihanna rolling her eyes",
+  },
+  {
+    code: 407,
+    title: "Proxy Authentication Required",
+    description:
+      "Rihanna standing infront of a bar counter saying 'I don't even know you like that'",
+  },
+  {
+    code: 409,
+    title: "Conflict",
+    description: "Rihanna hitting an ATM machine",
+  },
+  {
+    code: 410,
+    title: "Gone",
+    description: "Rihanna waving her hand saying 'Just wanna forget about it'",
+  },
+  { code: 418, title: "I'm a teapot", description: "Rihanna sipping tea" },
+  {
+    code: 426,
+    title: "Upgrade required",
+    description: "Rihanna putting a crown on her head!",
+  },
+  {
+    code: 429,
+    title: "Too many requests",
+    description: "Rihanna standing up and saying 'I don't like you right now'",
+  },
+  {
+    code: 503,
+    title: "Service unavailable",
+    description: "Rihanna sitting by the edge of the pool waving",
+  },
 ];
 
 export default statuses;
