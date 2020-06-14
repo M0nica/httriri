ff186f0d6e0932a08204a8044d28d29013e1d58b
Monica Powell <monica@aboutmonica.com>
2020-06-13 20:21:34 -0400

    make card a figure instead of div

--- components/statusCodeCard.tsx
+++ components/statusCodeCard.tsx
@@ -2,16 +2,18 @@ import styles from "./statusCodeCard.module.css";
 
 export default function StatusCodeCard({ code, title, description }) {
   return (
-    <div className={styles.card}>
+    <figure className={styles.card}>
       <img
         className={styles.gif}
         src={`/images/${code}.gif`}
         alt={description}
       />
 
-      <a className={styles.link} href={`https://httpstatuses.com/${code}`}>
-        {code}: {title} &#x2197;
-      </a>
-    </div>
+      <figcaption>
+        <a className={styles.link} href={`https://httpstatuses.com/${code}`}>
+          {code}: {title} &#x2197;
+        </a>
+      </figcaption>
+    </figure>
   );
 }
