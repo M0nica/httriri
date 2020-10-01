62b49ef02c4af823364cc82e8ede21fba382bc49
hulyak <57311842+hulya95@users.noreply.github.com>
2020-10-01 16:35:21 -0700

    599 : Network Connect Timeout Error

--- utils/statuses.ts
+++ utils/statuses.ts
@@ -1,103 +1,113 @@
 const statuses = [
   {
     code: 100,
-    title: "Continue",
+    title: 'Continue',
     description: "Rihanna with red hair asking if 'You ready?'",
   },
   {
     code: 101,
-    title: "Switching Protocols",
-    description: "Rihanna splashing water around in a music video",
+    title: 'Switching Protocols',
+    description: 'Rihanna splashing water around in a music video',
   },
   {
     code: 102,
-    title: "Processing",
-    description: "Rihanna tapping the side of her head with her finger, in thought.",
+    title: 'Processing',
+    description:
+      'Rihanna tapping the side of her head with her finger, in thought.',
   },
   {
     code: 200,
-    title: "OK",
+    title: 'OK',
     description:
-      "Rihanna nodding her head while cheering on Lizzo at the BET Awards 2019",
+      'Rihanna nodding her head while cheering on Lizzo at the BET Awards 2019',
   },
   {
     code: 201,
-    title: "Created",
-    description:
-      "Rihanna walking with joy in the Man Down video",
+    title: 'Created',
+    description: 'Rihanna walking with joy in the Man Down video',
   },
-  { code: 202, title: "Accepted", description: "Rihanna with a bob winking" },
-  { code: 206, title: "Partial Content", description: "Rihanna saying \"It's me in a collection\" while infront of a solid white background" },
+  {code: 202, title: 'Accepted', description: 'Rihanna with a bob winking'},
+  {
+    code: 206,
+    title: 'Partial Content',
+    description:
+      'Rihanna saying "It\'s me in a collection" while infront of a solid white background',
+  },
   {
     code: 300,
-    title: "Multiple Choices",
+    title: 'Multiple Choices',
     description:
-      "Rihanna dancing on stage with a pink outfit with an ensemble of background dancers behind her wearing outfits with the same shade of pink.",
+      'Rihanna dancing on stage with a pink outfit with an ensemble of background dancers behind her wearing outfits with the same shade of pink.',
   },
   {
     code: 301,
-    title: "Moved Permanently",
-    description: "Rihanna peacefully sipping from a coconut",
+    title: 'Moved Permanently',
+    description: 'Rihanna peacefully sipping from a coconut',
   },
   {
     code: 400,
-    title: "Bad Request",
+    title: 'Bad Request',
     description: "Rihanna shaking her finger 'no'",
   },
 
   {
     code: 402,
-    title: "Payment Required",
-    description: "Rihanna making hand gestures for money",
+    title: 'Payment Required',
+    description: 'Rihanna making hand gestures for money',
   },
   {
     code: 403,
-    title: "Forbidden",
+    title: 'Forbidden',
     description:
-      "Rihanna rolling her eyes at some one before her limo window rolls up",
+      'Rihanna rolling her eyes at some one before her limo window rolls up',
   },
   {
     code: 404,
-    title: "Not Found",
+    title: 'Not Found',
     description:
-      "Rihanna singing Story of my life Searching for the right But it keeps avoiding me",
+      'Rihanna singing Story of my life Searching for the right But it keeps avoiding me',
   },
   {
     code: 406,
-    title: "Not Acceptable",
-    description: "Rihanna rolling her eyes",
+    title: 'Not Acceptable',
+    description: 'Rihanna rolling her eyes',
   },
   {
     code: 407,
-    title: "Proxy Authentication Required",
+    title: 'Proxy Authentication Required',
     description:
       "Rihanna standing infront of a bar counter saying 'I don't even know you like that'",
   },
   {
     code: 409,
-    title: "Conflict",
-    description: "Rihanna hitting an ATM machine",
+    title: 'Conflict',
+    description: 'Rihanna hitting an ATM machine',
   },
   {
     code: 410,
-    title: "Gone",
+    title: 'Gone',
     description: "Rihanna waving her hand saying 'Just wanna forget about it'",
   },
-  { code: 418, title: "I'm a teapot", description: "Rihanna sipping tea" },
+  {code: 418, title: "I'm a teapot", description: 'Rihanna sipping tea'},
   {
     code: 426,
-    title: "Upgrade required",
-    description: "Rihanna putting a crown on her head!",
+    title: 'Upgrade required',
+    description: 'Rihanna putting a crown on her head!',
   },
   {
     code: 429,
-    title: "Too many requests",
+    title: 'Too many requests',
     description: "Rihanna standing up and saying 'I don't like you right now'",
   },
   {
     code: 503,
-    title: "Service unavailable",
-    description: "Rihanna sitting by the edge of the pool waving",
+    title: 'Service unavailable',
+    description: 'Rihanna sitting by the edge of the pool waving',
+  },
+  {
+    code: 599,
+    title: 'Network Connect Timeout Error',
+    description: 'Rihanna turning her head and looking angry',
   },
 ];
 
