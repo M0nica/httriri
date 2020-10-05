75d13b5204f3b34272d9111c7774b7a838468152
Monica Powell <monica@aboutmonica.com>
2020-10-04 22:23:57 -0400

    update React Fragment syntax

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,15 @@
 import styles from "./header.module.css";
+import React from "react"
 
 export default function Header() {
   return (
-    <>
+    <React.Fragment>
       <h1 className={styles.heading}>
         HTT<span className={styles.accent}>RIRI</span>
       </h1>
       <p className={styles.subheading}>
         Explore HTTP status codes with this collection of Rihanna GIFs
       </p>
-    </>
+    </React.Fragment>
   );
 }
