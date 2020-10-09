f580572923854d204a95e89fe2cd260f2fef41a2
Jabbar Memon <jabbar@zoop.one>
2020-10-09 23:37:25 +0530

    Added prettier && husky

--- utils/statuses.test.ts
+++ utils/statuses.test.ts
@@ -1,18 +1,17 @@
-import {statuses} from "./statuses";
+import { statuses } from "./statuses";
 
 describe("statuses", () => {
-  it("The statuses object is written in ascending order with smaller numbers first and larger numbers later", () => {
-   let prevStatus = 0;
-   for (let status of statuses){
-    expect(status.code).toBeGreaterThan(prevStatus);
-        prevStatus = status.code;
-   }
-  });
-  it("There should not be any duplicate status codes in the statuses object", () => {
-    statuses.reduce((acc, {code}) => {
-        expect(acc[code]).toBe(undefined);
-            return {code: 'seen', ...acc, }
-    }
-    , {})
-   });
+    it("The statuses object is written in ascending order with smaller numbers first and larger numbers later", () => {
+        let prevStatus = 0;
+        for (let status of statuses) {
+            expect(status.code).toBeGreaterThan(prevStatus);
+            prevStatus = status.code;
+        }
+    });
+    it("There should not be any duplicate status codes in the statuses object", () => {
+        statuses.reduce((acc, { code }) => {
+            expect(acc[code]).toBe(undefined);
+            return { code: "seen", ...acc };
+        }, {});
+    });
 });
