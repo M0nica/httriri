93fad54178afbf9a5eb882e9b4f47bce0d4de287
Monica Powell <github@aboutmonica.com>
2022-05-14 09:14:48 -0400

    resolve merge conflict

--- utils/statuses.ts
+++ utils/statuses.ts
@@ -11,23 +11,27 @@ export const statuses = [
   },
   {
     code: 102,
-    title: 'Processing',
-    description:
-      'Rihanna tapping the side of her head with her finger, in thought.',
+    title: "Processing",
+    description: "Rihanna tapping the side of her head with her finger, in thought.",
   },
   {
     code: 200,
-    title: 'OK',
-    description:
-      'Rihanna nodding her head while cheering on Lizzo at the BET Awards 2019',
+    title: "OK",
+    description: 'Rihanna tapping the side of her head with her finger, in thought.',
+  }, {
+    code: 201,
+    title: "Created",
+    description: "Rihanna walking with joy in the Man Down video",
   },
   {
-    code: 201,
-    title: 'Created',
-    description: 'Rihanna walking with joy in the Man Down video',
+    code: 202, title: "Accepted", description: "Rihanna with a bob winking"
   },
-  {code: 202, title: 'Accepted', description: 'Rihanna with a bob winking'},
-  { code: 203, title: "Non-Authoritative Information", description: "Rihanna showing her nails" },
+  {
+    code: 203, title: "Non-Authoritative Information", description: "Rihanna showing her nails"
+  },
+  {
+    code: 204, title: "No Content", description: "Rihanna sitting and saying why"
+  },
   {
     code: 206,
     title: 'Partial Content',
@@ -58,87 +62,87 @@ export const statuses = [
   },
   {
     code: 400,
-		title: "Bad Request",
-		description: "Rihanna shaking her finger 'no'",
-	},
-	{
-		code: 402,
-		title: "Payment Required",
-		description: "Rihanna making hand gestures for money",
-	},
-	{
-		code: 403,
-		title: "Forbidden",
-		description:
-			"Rihanna rolling her eyes at some one before her limo window rolls up",
-	},
-	{
-		code: 404,
-		title: "Not Found",
-		description:
-			"Rihanna singing Story of my life Searching for the right But it keeps avoiding me",
-	},
-	{
-		code: 406,
-		title: "Not Acceptable",
-		description: "Rihanna rolling her eyes",
-	},
-	{
-		code: 407,
-		title: "Proxy Authentication Required",
-		description:
-			"Rihanna standing infront of a bar counter saying 'I don't even know you like that'",
-	},
-	{
-		code: 409,
-		title: "Conflict",
-		description: "Rihanna hitting an ATM machine",
-	},
-	{
-		code: 410,
-		title: "Gone",
-		description:
-			"Rihanna waving her hand saying 'Just wanna forget about it'",
-	},
-	{ code: 418, title: "I'm a teapot", description: "Rihanna sipping tea" },
-	{
-		code: 426,
-		title: "Upgrade required",
-		description: "Rihanna putting a crown on her head!",
-	},
-	{
-		code: 429,
-		title: "Too many requests",
-		description:
-			"Rihanna standing up and saying 'I don't like you right now'",
-	},
-	{
-		code: 444,
-		title: "Connection closed without response",
-		description:
-			"Angry Rihanna violently hanging up and slamming phone down",
-	},
+    title: "Bad Request",
+    description: "Rihanna shaking her finger 'no'",
+  },
+  {
+    code: 402,
+    title: "Payment Required",
+    description: "Rihanna making hand gestures for money",
+  },
+  {
+    code: 403,
+    title: "Forbidden",
+    description:
+      "Rihanna rolling her eyes at some one before her limo window rolls up",
+  },
+  {
+    code: 404,
+    title: "Not Found",
+    description:
+      "Rihanna singing Story of my life Searching for the right But it keeps avoiding me",
+  },
+  {
+    code: 406,
+    title: "Not Acceptable",
+    description: "Rihanna rolling her eyes",
+  },
+  {
+    code: 407,
+    title: "Proxy Authentication Required",
+    description:
+      "Rihanna standing infront of a bar counter saying 'I don't even know you like that'",
+  },
+  {
+    code: 409,
+    title: "Conflict",
+    description: "Rihanna hitting an ATM machine",
+  },
+  {
+    code: 410,
+    title: "Gone",
+    description:
+      "Rihanna waving her hand saying 'Just wanna forget about it'",
+  },
+  { code: 418, title: "I'm a teapot", description: "Rihanna sipping tea" },
+  {
+    code: 426,
+    title: "Upgrade required",
+    description: "Rihanna putting a crown on her head!",
+  },
+  {
+    code: 429,
+    title: "Too many requests",
+    description:
+      "Rihanna standing up and saying 'I don't like you right now'",
+  },
+  {
+    code: 444,
+    title: "Connection closed without response",
+    description:
+      "Angry Rihanna violently hanging up and slamming phone down",
+  },
   {
     code: 500,
     title: "Internal server error",
     description: "Rihanna does not understand the question",
   },
-	{
-		code: 502,
-		title: "Bad gateway",
-		description: "Rihanna not impressed by the bad request",
-	},
-	{
-		code: 503,
-		title: "Service unavailable",
-		description: "Rihanna sitting by the edge of the pool waving",
-	},
-	{
-		code: 508,
-		title: "Loop detected",
-		description: "Rihanna moving her hands in a looping motion",
-	},
-   {
+  {
+    code: 502,
+    title: "Bad gateway",
+    description: "Rihanna not impressed by the bad request",
+  },
+  {
+    code: 503,
+    title: "Service unavailable",
+    description: "Rihanna sitting by the edge of the pool waving",
+  },
+  {
+    code: 508,
+    title: "Loop detected",
+    description: "Rihanna moving her hands in a looping motion",
+  },
+  {
     code: 599,
     title: 'Network Connect Timeout Error',
     description: 'Rihanna looking angry',
